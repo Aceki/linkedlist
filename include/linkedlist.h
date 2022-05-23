37eed896d277b622a7bd8084c3a0aebb2105a3c4
Brekhuntsov Nikita <aceki320@gmail.com>
2022-05-23 16:52:08 +0500

    Fix mistakes in 'linkedlist.h'

--- include/linkedlist.h
+++ include/linkedlist.h
@@ -18,19 +18,21 @@ public:
 
         Node(const Node&);
 
-        ~Node();
+        T& value();
 
         /// Возвращает значение, которое содержит узел.
-        T value() const;
+        const T& value() const;
 
         /// Задаёт значение узла.
         void setValue(const T& value);
 
-        /// Возращает следующий узел.
-        Node* next() const;
-
         /// Задаёт следующий узел
         void setNext(Node* node);
+
+        bool hasNext() const;
+
+        /// Возращает следующий узел.
+        Node* next() const;
 
         Node& operator=(const Node&);
 
@@ -100,8 +102,8 @@ public:
     LinkedList<T>& operator=(const LinkedList<T>&);
 
 private:
-    Node* m_first = nullptr;
-    Node* m_last = nullptr;
+    Node* m_firstNode = nullptr;
+    Node* m_lastNode = nullptr;
     int m_length = 0;
 };
 
@@ -125,8 +127,7 @@ LinkedList<T>::LinkedList(std::initializer_list<T> values)
 template <class T>
 inline LinkedList<T>::~LinkedList()
 {
-    if (!isEmpty())
-        delete m_first;
+    clear();
 }
 
 template <class T>
@@ -135,11 +136,11 @@ void LinkedList<T>::addFirst(const T& value)
     Node* node = nullptr;
     if (isEmpty()) {
         node = new Node(value);
-        m_first = m_last = node;
+        m_firstNode = m_lastNode = node;
     }
     else {
-        node = new Node(value, m_first);
-        m_first = node;
+        node = new Node(value, m_firstNode);
+        m_firstNode = node;
     }
 
     m_length++;
@@ -150,10 +151,10 @@ void LinkedList<T>::addLast(const T& value)
 {
     Node* node = new Node(value);
     if (isEmpty())
-        m_first = m_last = node;
+        m_firstNode = m_lastNode = node;
     else {
-        m_last->setNext(node);
-        m_last = node;
+        m_lastNode->setNext(node);
+        m_lastNode = node;
     }
 
     m_length++;
@@ -162,13 +163,15 @@ void LinkedList<T>::addLast(const T& value)
 template <class T>
 void LinkedList<T>::addBefore(const Node* nextNode, const T& value)
 {
+    if (nextNode == nullptr)
+        throw std::logic_error("`nextNode` should not equals `nullptr`.");
     if (isEmpty())
         throw std::logic_error("List is empty.");
 
-    if (nextNode == m_first)
+    if (nextNode == m_firstNode)
         addFirst(value);
     else {
-        Node* previousNode = m_first;
+        Node* previousNode = m_firstNode;
         while (previousNode != nullptr && previousNode->next() != nextNode)
             previousNode = previousNode->next();
 
@@ -185,14 +188,16 @@ void LinkedList<T>::addBefore(const Node* nextNode, const T& value)
 template <class T>
 void LinkedList<T>::addAfter(const Node* previousNode, const T& value)
 {
+    if (previousNode == nullptr)
+        throw std::logic_error("`previousNode` should not equals `nullptr`.");
     if (isEmpty())
         throw std::logic_error("List is empty.");
 
-    if (previousNode == m_last)
+    if (previousNode == m_lastNode)
         addLast(value);
     else {
-        Node* nextNode = m_first;
-        while (nextNode != previousNode->next() && nextNode != nullptr)
+        Node* nextNode = m_firstNode;
+        while (nextNode != nullptr && nextNode != previousNode->next())
             nextNode = nextNode->next();
 
         if (nextNode == nullptr)
@@ -214,11 +219,10 @@ void LinkedList<T>::removeFirst()
     if (length() == 1) {
         clear();
     } else {
-        Node* first = m_first;
-        m_first = first->next();
-        first->setNext(nullptr);
+        Node* firstNode = m_firstNode;
+        m_firstNode = firstNode->next();
 
-        delete first;
+        delete firstNode;
 
         m_length--;
     }
@@ -233,14 +237,14 @@ void LinkedList<T>::removeLast()
     if (length() == 1) {
         clear();
     } else {
-        Node* last = m_last;
-        m_last = m_first;
-        while (m_last->next() != last)
-            m_last = m_last->next();
+        Node* lastNode = m_lastNode;
+        m_lastNode = m_firstNode;
+        while (m_lastNode->next() != lastNode)
+            m_lastNode = m_lastNode->next();
 
-        m_last->setNext(nullptr);
+        m_lastNode->setNext(nullptr);
 
-        delete last;
+        delete lastNode;
 
         m_length--;
     }
@@ -252,24 +256,23 @@ void LinkedList<T>::remove(const T& value)
     if (isEmpty())
         throw std::logic_error("List is empty.");
 
-    if (value == m_first->value())
+    if (value == m_firstNode->value())
         removeFirst();
     else {
         if (length() == 1)
             clear();
         else {
-            Node* previousNode = m_first;
-            while (previousNode->next() != nullptr && previousNode->next()->value() != value)
+            Node* previousNode = m_firstNode;
+            while (previousNode->hasNext() && previousNode->next()->value() != value)
                 previousNode = previousNode->next();
 
-            if (previousNode == m_last)
+            if (previousNode == m_lastNode)
                 throw std::logic_error("List does not contains `value`.");
 
             Node* node = previousNode->next();
             Node* nextNode = node->next();
 
             previousNode->setNext(nextNode);
-            node->setNext(nullptr);
 
             delete node;
 
@@ -283,26 +286,26 @@ void LinkedList<T>::remove(const Node* node)
 {
     if (isEmpty())
         throw std::logic_error("List is empty.");
+    if (node == nullptr)
+        throw std::logic_error("`node` should not equals `nullptr`.");
 
-    if (node == m_first)
+    if (node == m_firstNode)
         removeFirst();
-    else if (node == m_last)
+    else if (node == m_lastNode)
         removeLast();
     else {
         if (length() == 1) {
             clear();
         } else {
-            Node* previousNode = m_first;
-            while (previousNode->next() != nullptr && previousNode->next() != node)
+            Node* previousNode = m_firstNode;
+            while (previousNode->hasNext() && previousNode->next() != node)
                 previousNode = previousNode->next();
 
-            if (previousNode == m_last)
+            if (previousNode == m_lastNode)
                 throw std::logic_error("List does not contains `node`.");
 
             Node* nextNode = node->next();
-
             previousNode->setNext(nextNode);
-            const_cast<Node*>(node)->setNext(nullptr);
 
             delete node;
 
@@ -317,7 +320,7 @@ const typename LinkedList<T>::Node* LinkedList<T>::find(const T& value) const
     if (isEmpty())
         return nullptr;
 
-    Node* result = m_first;
+    Node* result = m_firstNode;
     while (result != nullptr && result->value() != value)
         result = result->next();
 
@@ -331,7 +334,7 @@ const typename LinkedList<T>::Node* LinkedList<T>::findLast(const T& value) cons
         return nullptr;
 
     Node* result = nullptr;
-    Node* node = m_first;
+    Node* node = m_firstNode;
     while (node != nullptr) {
         if (node->value() == value)
             result = node;
@@ -348,7 +351,7 @@ bool LinkedList<T>::contains(const T& value) const
     if (isEmpty())
         return false;
 
-    Node* node = m_first;
+    Node* node = m_firstNode;
     while (node != nullptr && node->value() != value)
         node = node->next();
 
@@ -358,9 +361,15 @@ bool LinkedList<T>::contains(const T& value) const
 template <class T>
 void LinkedList<T>::clear()
 {
-    if (m_first != nullptr) {
-        delete m_first;
-        m_first = m_last = nullptr;
+    if (!isEmpty()) {
+        Node* previousNode = m_firstNode;
+        while (previousNode->hasNext()) {
+            Node* nextNode = previousNode->next();
+            delete previousNode;
+            previousNode = nextNode;
+        }
+        delete previousNode;
+        m_firstNode = m_lastNode = nullptr;
         m_length = 0;
     }
 }
@@ -368,13 +377,13 @@ void LinkedList<T>::clear()
 template <class T>
 inline const typename LinkedList<T>::Node* LinkedList<T>::first() const
 {
-    return m_first;
+    return m_firstNode;
 }
 
 template <class T>
 inline const typename LinkedList<T>::Node* LinkedList<T>::last() const
 {
-    return m_last;
+    return m_lastNode;
 }
 
 template <class T>
@@ -414,14 +423,13 @@ template <class T>
 LinkedList<T>::Node::Node(const Node& obj) : m_value(obj.m_value) { }
 
 template <class T>
-LinkedList<T>::Node::~Node()
+inline T& LinkedList<T>::Node::value()
 {
-    if (m_next != nullptr)
-        delete m_next;
+    return m_value;
 }
 
 template <class T>
-inline T LinkedList<T>::Node::value() const 
+inline const T& LinkedList<T>::Node::value() const 
 {
     return m_value;
 }
@@ -433,15 +441,21 @@ inline void LinkedList<T>::Node::setValue(const T& value)
 }
 
 template <class T>
-inline typename LinkedList<T>::Node* LinkedList<T>::Node::next() const
-{
-    return m_next;
-}
-
-template <class T>
 inline void LinkedList<T>::Node::setNext(Node* node)
 {
     m_next = node;
+}
+
+template <class T>
+inline bool LinkedList<T>::Node::hasNext() const
+{
+    return m_next != nullptr;
+}
+
+template <class T>
+inline typename LinkedList<T>::Node* LinkedList<T>::Node::next() const
+{
+    return m_next;
 }
 
 template <class T>
